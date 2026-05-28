9286cf6b0ae3348b0a31964bb7cad1c190063ff4
khrystyna.stegnii <khrystyna.stegnii@gmail.com>
2026-05-28 23:15:23 +0300

    added jsFunctions folder with functions

---
+++ jsFunctions/task1.js
@@ -0,0 +1,17 @@
+function getArea (width, height){
+ let result = width * height;
+ return result;
+}
+console.log(getArea(5, 10));
+
+const area = function (width, height){
+ let result = width * height;
+ return result;
+}   
+console.log(area(5, 10));
+
+const area1 = (width, height) => {
+ let result = width * height;
+ return result;
+}
+console.log(area1(5, 10));
